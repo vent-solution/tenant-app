7419b925cd20f8a5609d6d1c72f82588a01a7dc6
Anatoli <you@example.com>
2025-05-22 15:36:08 +0300

    Anatoli

--- src/other/Preloader.tsx
+++ src/other/Preloader.tsx
@@ -10,13 +10,25 @@ const Preloader: React.FC<Props> = () => {
 
   return (
     <div className="preloader">
-      <div className="cube text-white text-xl font-bold">
-        <div className="side side1 flex justify-center items-center">Vent</div>
-        <div className="side side2 flex justify-center items-center">Vent</div>
-        <div className="side side3 flex justify-center items-center">Vent</div>
-        <div className="side side4 flex justify-center items-center">Vent</div>
-        <div className="side side5 flex justify-center items-center">Vent</div>
-        <div className="side side6 flex justify-center items-center">Vent</div>
+      <div className="cube text-white text-lg font-bold">
+        <div className="side side1 flex justify-center items-center p-1">
+          Vent
+        </div>
+        <div className="side side2 flex justify-center items-center p-1">
+          Vent
+        </div>
+        <div className="side side3 flex justify-center items-center p-1">
+          Vent
+        </div>
+        <div className="side side4 flex justify-center items-center p-1">
+          Vent
+        </div>
+        <div className="side side5 flex justify-center items-center p-1">
+          Vent
+        </div>
+        <div className="side side6 flex justify-center items-center p-1">
+          Vent
+        </div>
       </div>
     </div>
   );
